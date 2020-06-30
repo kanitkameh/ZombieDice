1e29bdeab872eb8ed598659fa8dd1c708e7b639b
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-06-30 21:03:58 +0300

    add dice pool

---
+++ Sources/ZombieDice/DicePool.swift
@@ -0,0 +1,23 @@
+class DicePool{
+    var dices: [Dice];
+    //Creating dice pool of 3 red, 4 yellow and 6 green dices
+    init(){
+        dices = [];
+        let reds = Array(repeating: Dice.Red, count: 3)
+        let yellows = Array(repeating: Dice.Yellow, count: 4)
+        let greens = Array(repeating: Dice.Green, count: 6)
+        dices+=(reds);
+        dices+=(yellows);
+        dices+=(greens);
+    }
+    //Draws a dice if there is one in the dice pool
+    func drawDice() -> Dice? {
+        if(dices.isEmpty){
+            return nil;
+        }
+        let position = Int.random(in: 0..<dices.count)
+        let result: Dice? = dices[position];
+        dices.remove(at: position);
+        return result;
+    }
+}
