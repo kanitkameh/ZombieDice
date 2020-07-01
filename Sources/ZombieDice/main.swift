26610c01f5023c390f37f2f43ba79fe6c8aef3ee
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-07-01 20:28:58 +0300

    make clear visible earlier

--- Sources/ZombieDice/main.swift
+++ Sources/ZombieDice/main.swift
@@ -1,14 +1,15 @@
+//ANSI sequence where \u{001B} is ESCAPE and [2J is clear screen.
+func clearScreen(){
+    print("\u{001B}[2J");
+}
 var playAgain: Bool;
 print("Enter player names:")
 var playerNames:[String]? = readLine()?.split(separator: " ").map { String($0) };
 
 var game = Game(playerNames:playerNames ?? ["Unnamed Player"])
 repeat {
+    clearScreen();
     game.beginGame();
     print("Do you want to play again?(yes/no)")
     playAgain = (readLine()=="yes");
 } while playAgain
-//ANSI sequence where \u{001B} is ESCAPE and [2J is clear screen.
-func clearScreen(){
-    print("\u{001B}[2J");
-}
